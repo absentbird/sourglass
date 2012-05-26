09ffe98aebe5362b27aeb3313b793f28534b0d21
Shawn Bird <absent.bird@gmail.com>
2012-05-25 20:28:24 -0700

    first working version

--- timetrack.py
+++ timetrack.py
@@ -4,7 +4,7 @@ import csv
 
 parser = argparse.ArgumentParser(description='Log time tracking information')
 parser.add_argument('-p', '--project', metavar='p', type=str, action='store', dest='project', help='The project name. Defaults to last project opened')
-parser.add_argument('memo', metavar='m', type=str, nargs='?', help='A memo of what was done')
+parser.add_argument('memo', metavar='m', type=str, nargs='?', default='', help='A memo of what was done')
 
 arguments = parser.parse_args()
 
@@ -15,24 +15,34 @@ except IOError as e:
     print 'No settings found.', 'Would you like to make a new settings file?'
     exit()
 else:
-    last = open('last', 'r')
-    last = last.read().partition(' ')
+    store = open('last', 'r')
+    last = store.readline().partition(' ')
+    store.close()
 
 if arguments.project:
     path = arguments.project
+    store = open('last', 'w')
+    store.write(path + ' a')
+    store.close
 else:
-    if state.last:
+    if last:
         path = last[0]
     else:
-        print "No project specified"
+        print "No project specified.", "Run again with -p"
         exit()
 
-def loadlog(path):
-    try:
-        open()
-    except IOError as e:
-        print "Project not found.", "Start new project?"
-        exit()
+try:
+    open(path)
+except IOError as e:
+    print "Project not found.", "Start new project?"
+    exit()
+else:
+    log = open(path, 'a')
 
-def logtime(path, memo):
-    pass
+if last:
+    status = last[2].rstrip('\n')
+else:
+    status = 'a'
+
+writer = csv.writer(log)
+writer.writerow((time.time(), status, arguments.memo))
